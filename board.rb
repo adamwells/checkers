2032e98ac5ff079517f32a31cf4036422c522bd1
Adam Wells <adamwells.dev@gmail.com>
2015-02-26 17:23:45 -0800

    Fix indentation

--- board.rb
+++ board.rb
@@ -3,82 +3,82 @@ require_relative 'piece'
 require 'byebug'
 
 class Board
-	GRID_SIZE = 8
-	ROWS = 3
-	attr_reader :grid
+  GRID_SIZE = 8
+  ROWS = 3
+  attr_reader :grid
 
-	def initialize
-		@grid = Array.new(GRID_SIZE) { Array.new(GRID_SIZE) }
-	end
+  def initialize
+    @grid = Array.new(GRID_SIZE) { Array.new(GRID_SIZE) }
+  end
 
-	def setup_board
-		grid.each_with_index do |row, i|
-			row.each_with_index do |position, j|
-				if (i + j).odd? && i < ROWS
-					grid[i][j] = Piece.new(:green, [i, j], self)
-				elsif (i + j).odd? && i >= GRID_SIZE - ROWS
-					grid[i][j] = Piece.new(:yellow, [i, j], self)
-				end
-			end
-		end
-	end
+  def setup_board
+    grid.each_with_index do |row, i|
+      row.each_with_index do |position, j|
+        if (i + j).odd? && i < ROWS
+          grid[i][j] = Piece.new(:green, [i, j], self)
+        elsif (i + j).odd? && i >= GRID_SIZE - ROWS
+          grid[i][j] = Piece.new(:yellow, [i, j], self)
+        end
+      end
+    end
+  end
 
-	def render
-		grid.each_with_index do |row, i|
-			print "#{GRID_SIZE - (i + 1)} "
-			row.each_with_index do |position, j|
-				background = ((i + j).even? ? :white : :black)
-				if self[[i, j]].nil?
-					print '  '.colorize(:background => background)
-				else
-					print self[[i, j]].show.colorize(:background => background)
-				end
-			end
-			puts
-		end
-		puts "   0 1 2 3 4 5 6 7"
-		nil
-	end
+  def render
+    grid.each_with_index do |row, i|
+      print "#{GRID_SIZE - (i + 1)} "
+      row.each_with_index do |position, j|
+        background = ((i + j).even? ? :white : :black)
+        if self[[i, j]].nil?
+          print '  '.colorize(:background => background)
+        else
+          print self[[i, j]].show.colorize(:background => background)
+        end
+      end
+      puts
+    end
+    puts "   0 1 2 3 4 5 6 7"
+    nil
+  end
 
-	def make_move_sequence(sequence)
-		raise ArgumentError.new('Must start on a valid piece!') if self[sequence[0]].nil?
-		self[sequence[0]].perform_moves(sequence)
-	end
+  def make_move_sequence(sequence)
+    raise ArgumentError.new('Must start on a valid piece!') if self[sequence[0]].nil?
+    self[sequence[0]].perform_moves(sequence)
+  end
 
-	def move(start_pos, end_pos)
-		place_piece(end_pos, self[start_pos])
-		self[start_pos] = nil
-	end
+  def move(start_pos, end_pos)
+    place_piece(end_pos, self[start_pos])
+    self[start_pos] = nil
+  end
 
-	def place_piece(pos, piece)
-		self[pos] = piece
-		piece.board = self
-		piece.position = pos
-	end
+  def place_piece(pos, piece)
+    self[pos] = piece
+    piece.board = self
+    piece.position = pos
+  end
 
-	def over?
-		return false
-	end
+  def over?
+    return false
+  end
 
-	def dup
-		duped_board = Board.new
-		@grid.flatten.compact.each do |piece|
-			duped_board[piece.position] = Piece.new(piece.color, piece.position, duped_board, piece.king?)
-		end
-		duped_board
-	end
+  def dup
+    duped_board = Board.new
+    @grid.flatten.compact.each do |piece|
+      duped_board[piece.position] = Piece.new(piece.color, piece.position, duped_board, piece.king?)
+    end
+    duped_board
+  end
 
-	def size
-		GRID_SIZE
-	end
+  def size
+    GRID_SIZE
+  end
 
-	def [](pos)
-		x, y = pos
-		grid[x][y]
-	end
+  def [](pos)
+    x, y = pos
+    grid[x][y]
+  end
 
-	def []=(pos, piece)
-		x, y = pos
-		grid[x][y] = piece
-	end
+  def []=(pos, piece)
+    x, y = pos
+    grid[x][y] = piece
+  end
 end
--- checkers.rb
+++ checkers.rb
@@ -1,66 +1,66 @@
 require_relative 'board'
 
 class Checkers
-	attr_accessor :board
+  attr_accessor :board
 
-	def initialize
-		@board = Board.new
-		@board.setup_board
-		@turn = :yellow
-	end
+  def initialize
+    @board = Board.new
+    @board.setup_board
+    @turn = :yellow
+  end
 
-	def play
-		until board.over?
-			system('clear')
-			board.render
-			sequence = get_move_sequence
+  def play
+    until board.over?
+      system('clear')
+      board.render
+      sequence = get_move_sequence
 
-			raise ArgumentError.new('Cannot move pieces belonging to the other player!') if board[sequence[0]].color != @turn
-			board.make_move_sequence(sequence)
+      raise ArgumentError.new('Cannot move pieces belonging to the other player!') if board[sequence[0]].color != @turn
+      board.make_move_sequence(sequence)
 
-			@turn = (@turn == :yellow ? :green : :yellow)
-		end
+      @turn = (@turn == :yellow ? :green : :yellow)
+    end
 
-	rescue InvalidMoveError => e
-		puts e.message
-		sleep(2)
-		retry
-	rescue ArgumentError => e
-		puts e.message
-		sleep(2)
-		retry
-	rescue InvalidMoveError => e
-		puts "That move sequence doesn't work!"
-		sleep(2)
-		retry
-	rescue => e
-		puts "Something went wrong... Sorry!"
-		sleep(2)
-		retry
-	end
+  rescue InvalidMoveError => e
+    puts e.message
+    sleep(2)
+    retry
+  rescue ArgumentError => e
+    puts e.message
+    sleep(2)
+    retry
+  rescue InvalidMoveError => e
+    puts "That move sequence doesn't work!"
+    sleep(2)
+    retry
+  rescue => e
+    puts "Something went wrong... Sorry!"
+    sleep(2)
+    retry
+  end
 
-	def parse!(sequence)
-		sequence.pop
-		sequence.map! do |el|
-			el.map { |num| num.to_i }
-		end
+  def parse!(sequence)
+    sequence.pop
+    sequence.map! do |el|
+      el.map { |num| num.to_i }
+    end
 
-		sequence.map! { |el| [7 - el[0], el[1]]}
-	end
+    sequence.map! { |el| [7 - el[0], el[1]]}
+  end
 
-	def get_move_sequence
-		sequence = []
-		puts "What is the position of the piece you'd like to move, #{@turn}? (<x>, <y> format)".colorize(@turn)
-		sequence << gets.chomp.split(',').reverse
+  def get_move_sequence
+    sequence = []
+    puts "What is the position of the piece you'd like to move, #{@turn}? (<x>, <y> format)".colorize(@turn)
+    sequence << gets.chomp.split(',').reverse
 
-		puts "Enter first/next move position, #{@turn} ('q' to end sequence).".colorize(@turn)
-		until sequence.last == ['q']
-			sequence << gets.chomp.split(',').reverse
-			p sequence
-		end
+    puts "Enter first/next move position, #{@turn} ('q' to end sequence).".colorize(@turn)
+    until sequence.last == ['q']
+      sequence << gets.chomp.split(',').reverse
+      p sequence
+    end
 
-		parse!(sequence)
-	end
+    parse!(sequence)
+  end
 end
 
 c = Checkers.new
